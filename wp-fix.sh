bf21affaf83ccc6af4976908bd8e5f746bd4c859
Zach Baker <zbakerms@hotmail.com>
2018-08-01 20:56:14 +0000

    asdf

--- wp-fix.sh
+++ wp-fix.sh
@@ -4,75 +4,15 @@ MYSQLUSER=$MYSQLUSER || "mysqluser"
 MYSQLPASSWORD=$MYSQLPASS || "mysqlpassword"
 MYSQLDB=$MYSQLDB || $MYSQLUSER
 CNAME=$CNAME || ""
-# DEBUg=1
+DEBUG=1
 VERBOSE=1
 RANDVAL=$RANDOM
 BKDIR=$HOME/.backup
+if [[ ! -d $BKDIR ]]; then mkdir $BKDIR; fi
 WPCLI=0
 DT=$(date +'%m-%d-%y-%H-%M-%s')
+ACTIONFOUND=0
 ## Init functions
-if [[ $# -lt 1 || $# -gt 2 ]]; then
- echo "$0: Usage:\r\n#1: $0 <mysql/application userame>\r\n#2: $0 <mysqluser> <mysqlpassword>\r\n" >/dev/stderr
- exit 1
-fi
-if [[ $# -eq 1 ]]; then
- if [[ -z $MYPATH || ! -d "$MYPATH" ]]; then MYPATH="$HOME/applications/$1/public_html"; fi
- if [[ ! -d "$MYPATH" ]]; then
-  echo "Unable to find path $MYPATH" >/dev/stderr
-  exit 1
- fi
-
- if [[ ! -e "$MYPATH/wp-config.php" ]]; then
-  echo "Unable to find wp-config" >/dev/stderr
-  exit 1
- fi
- if [[ -e "$MYPATH/wp-cli.phar" ]]; then
-  WPCLI=1
- fi
-
-
- MYSQLUSER=$(grep -e '^.*define.*DB_USER.*$' $MYPATH/wp-config.php |         sed -e "s/^.*'\(.*\)',.*'\([a-z|A-Z|0-9]*\)');.*$/\2/g" )
- MYSQLPASS=$(grep -e '^.*define.*DB_PASSWORD.*$' $MYPATH/wp-config.php | sed -e "s/^.*'\(.*\)',.*'\([a-z|A-Z|0-9]*\)');.*$/\2/g" )
-elif [[ $# -eq 2 ]]; then
- MYSQLUSER=$1
- MYSQLPASS=$2
-fi 
-# MYPATH=$4 || "/path/to"
- MYPATH="$HOME/applications/$MYSQLUSER/public_html"
- if [[ ! -d $MYPATH ]]; then echo "Path: $MYPATH doesn't exist. Aborting." >/dev/stderr; exit 1; fi
- echo "Creating my.sed and my.query" 
- echo 's/wordpress-\([\d]{,6}\)-\([\d]{,6}\)\./'$CNAME'/g' > $HOME/bin/my.sed
- echo 's/home\/.*\/'$MYSQLUSER'/home\/master\/applications\/'$MYSQLUSER'/g' >> $HOME/bin/my.sed
- echo "select option_value from wp_options where option_value LIKE 'http%\/\/wordpress-%-%.%' limit 1;" > $HOME/bin/my.query 
- MYQUERY=$(mysql -u$MYSQLUSER -p$MYSQLPASS $MYSQLUSER < ~/bin/my.query)
- if [[ $? -ne 0 ]]; then echo "Unable to connect to mysql." >/dev/stderr;exit 1; fi
- CNAME=$(echo "$MYQUERY" |tail -1|sed -e 's/^ht.*\(wordpress-[0-9]\{6\}-[0-9]\{6\}\).*$/\1/g')
- if [[ -z $VERBOSE ]]; then echo "Found User: $MYSQLUSER\tPass: $MYSQLPASS\tCNAME: $CNAME\tMYPATH: $MYPATH"; fi
-
- if  [[ -z $DEBUG ]]; then
-  MSED=$(cat ~/bin/my.sed)
-  echo -e "my.sed: $MSED"
-  echo "mysqluser: $MYSQLUSER pass: $MYSQLPASS cname=$CNAME PATH=$MYPATH"
- fi
- if [[ -z $CNAME ]]; then echo "Cname is blank" >/dev/stderr; exit 1; fi
-
-function mysql_dump
-{
- BACKUP=1
- ARCHIVE=1
- DUMPPATH=$BKDIR'/.mysql-'$MYSQLUSER'-'$DT'.sql'
- DUMPPATHBK=$DUMPPATH'.bk'
- mysqldump -u $MYSQLUSER --all-databases -p$MYSQLPASS > $DUMPPATH
- cp "$DUMPPATH" "$DUMPPATHBK"
- if [[ $WRITE == 1 ]]; then
-  sed -i -e 's/wordpress-[0-9]\{6\}-[0-9]\{6\}/'$CNAME'/g' $DUMPPATH
-  sed -i -f $HOME/bin/my.sed $DUMPPATH
-  mysql -u $MYSQLUSER -p$MYSQLPASS $MYSQLUSER < $DUMPPATH
- fi
- if [[ -e $DUMPPATH ]] && [[ -z $ARCHIVE || $ARCHIVE -eq 0 ]]; then rm -rf $DUMPPATH; fi
- if [[ -e $DUMPPATHBK ]] && [[ -z $BACKUP || $BACKUP -eq 0 ]]; then rm -rf $DUMPPATHBK;  fi
-}
-
 function file_change
 {
  OIFS=$IFS
@@ -115,6 +55,23 @@ function set_debug
  cd "$OPWD"
 }
  
+function mysql_dump
+{
+ BACKUP=1
+ ARCHIVE=1
+ DUMPPATH=$BKDIR'/.mysql-'$MYSQLUSER'-'$DT'.sql'
+ DUMPPATHBK=$DUMPPATH'.bk'
+ mysqldump -u $MYSQLUSER --all-databases -p$MYSQLPASS > $DUMPPATH
+ cp "$DUMPPATH" "$DUMPPATHBK"
+ if [[ $WRITE == 1 ]]; then
+  sed -i -e 's/wordpress-[0-9]\{6\}-[0-9]\{6\}/'$CNAME'/g' $DUMPPATH
+  sed -i -f $HOME/bin/my.sed $DUMPPATH
+  mysql -u $MYSQLUSER -p$MYSQLPASS $MYSQLUSER < $DUMPPATH
+ fi
+ if [[ -e $DUMPPATH ]] && [[ -z $ARCHIVE || $ARCHIVE -eq 0 ]]; then rm -rf $DUMPPATH; fi
+ if [[ -e $DUMPPATHBK ]] && [[ -z $BACKUP || $BACKUP -eq 0 ]]; then rm -rf $DUMPPATHBK;  fi
+}
+
 function create_exports
 {
  if [[ $WPCLI == 1 && -x "$MYPATH/wp-cli.phar" ]]; then
@@ -126,9 +83,93 @@ function create_exports
  fi
  cd "$OPWD"
 }
+if [[ $# -lt 1 ]]; then # || $# -gt 2 ]]; then
+ echo "$0: Usage:\r\n#1: $0 <mysql/application userame>\r\n#2: $0 <mysqluser> <mysqlpassword>\r\n" >/dev/stderr
+ exit 1
+fi
+if [[ $# -gt 1 ]]; then
+ P2=$2
+ ACTIONFOUND=1
+ echo "p2: $P2"
+ if [[ ${P2:0:7} == "ACTION:" ]]; then
+  FUNC=${P2:7}
+  echo "fund $FUNC"
+  if [[ ! -z $FUNC ]]; then
+   case $FUNC in
+    mysql)
+	mysql_dump
+	echo "dumped"
+	;;
+    files)
+ 	echo "files changed"
+	file_change
+	mod_files
+	;;
+    debug|debug:true)
+	echo "Setting debug"
+	set_debug
+	;;
+     debug:false)
+	echo "setting debug false"
+	set_debug false
+	;;
+     default|*)
+	echo "Invalid action: $FUNC -- Try mysql or files"
+	;;
+     esac
+   fi  
+ fi
+fi
+#mysql_dump
+#file_change
+#mod_files
+
+  
+  
+if [[ $# -eq 1 || $ACTIONFOUND -eq 1 ]]; then
+ if [[ -z $MYPATH || ! -d "$MYPATH" ]]; then MYPATH="$HOME/applications/$1/public_html"; fi
+ if [[ ! -d "$MYPATH" ]]; then
+  echo "Unable to find path $MYPATH" >/dev/stderr
+  exit 1
+ fi
+
+ if [[ ! -e "$MYPATH/wp-config.php" ]]; then
+  echo "Unable to find wp-config" >/dev/stderr
+  exit 1
+ fi
+ if [[ -e "$MYPATH/wp-cli.phar" ]]; then
+  WPCLI=1
+ fi
+
+
+ MYSQLUSER=$(grep -e '^.*define.*DB_USER.*$' $MYPATH/wp-config.php | sed -e "s/^.*'\(.*\)',.*'\([a-z|A-Z|0-9]*\)');.*$/\2/g" )
+ MYSQLPASS=$(grep -e '^.*define.*DB_PASSWORD.*$' $MYPATH/wp-config.php | sed -e "s/^.*'\(.*\)',.*'\([a-z|A-Z|0-9]*\)');.*$/\2/g" )
+elif [[ $# -eq 2 || $ACTIONFOUND -eq 0 ]]; then
+ MYSQLUSER=$1
+ MYSQLPASS=$2
+fi 
+if [[ ! -z $VERBOSE ]]; then echo "User: $MYSQLUSER Pass: $MYSQLPASS"; fi
+# MYPATH=$4 || "/path/to"
+ MYPATH="$HOME/applications/$MYSQLUSER/public_html"
+ if [[ ! -d $MYPATH ]]; then echo "Path: $MYPATH doesn't exist. Aborting." >/dev/stderr; exit 1; fi
+ echo 's/wordpress-\([\d]{,6}\)-\([\d]{,6}\)\./'$CNAME'/g' > $HOME/bin/my.sed
+ echo 's/home\/.*\/'$MYSQLUSER'/home\/master\/applications\/'$MYSQLUSER'/g' >> $HOME/bin/my.sed
+ echo "select option_value from wp_options where option_name='siteurl' LIMIT 1;" > $HOME/bin/my.query 
+ MYQUERY=$(mysql -u$MYSQLUSER -p$MYSQLPASS $MYSQLUSER < ~/bin/my.query)
+ if [[ $? -ne 0 ]]; then echo "Unable to connect to mysql." >/dev/stderr;exit 1; fi
+ CNAME=$(echo "$MYQUERY" |tail -1|sed -e 's/^ht.*\(wordpress-[0-9]\{6\}-[0-9]\{6\}\).*$/\1/g')
+ if [[ -z $VERBOSE ]]; then echo "Found User: $MYSQLUSER\tPass: $MYSQLPASS\tCNAME: $CNAME\tMYPATH: $MYPATH"; fi
+
+ if  [[ -z $DEBUG ]]; then
+  MSED=$(cat ~/bin/my.sed)
+  echo -e "my.sed: $MSED"
+  echo "mysqluser: $MYSQLUSER pass: $MYSQLPASS cname=$CNAME PATH=$MYPATH"
+ fi
+ if [[ -z $CNAME ]]; then echo "CNAME Is blank" >/dev/stderr; exit 1; fi
+
 
 # set_debug false
 #mysql_dump
 #file_change
 #mod_files
-# cleanup
+ cleanup
